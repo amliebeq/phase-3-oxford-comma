91271e7854de7c975f9c805040c432a58bc87c76
alex lieberman <alex.lieberman182@gmail.com>
2023-03-25 09:44:50 -0700

    commit tree f91803345dc0fa1e984061e5c9757c148d19329f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array = ["fiddleheads", "okra", "kohlrabi"])
+    if array.length == 1
+        array[0]
+    elsif array.length == 2
+        "#{array[0]} and #{array[1]}"
+    else        
+        new_array = array.slice(0, array.length - 1)
+        even_newer_array = new_array << "and #{array[array.length-1]}"
+        even_newer_array.join(', ')
+    end
 end
+
+oxford_comma
